18160923ae354abc4000c41d961aed5ffadd4acc
Ernesto Cejas <ernesto@near.org>
2022-08-31 12:11:06 -0400

    NO_JIRA: Set 'info' as default log level and allow 'RUST_LOG' to override

--- src/main.rs
+++ src/main.rs
@@ -64,8 +64,23 @@ async fn main() -> std::io::Result<()> {
     dotenv::dotenv().ok();
 
     let env_filter = tracing_subscriber::EnvFilter::new(
-        "near=info,near_jsonrpc_client=warn,near_enhanced_api=debug",
+        "info,near=info,near_jsonrpc_client=warn,near_enhanced_api=debug",
     );
+
+    if let Ok(rust_log) = std::env::var("RUST_LOG") {
+        if !rust_log.is_empty() {
+            for directive in rust_log.split(',').filter_map(|s| match s.parse() {
+                Ok(directive) => Some(directive),
+                Err(err) => {
+                    eprintln!("Ignoring directive `{}`: {}", s, err);
+                    None
+                }
+            }) {
+                env_filter = env_filter.add_directive(directive);
+            }
+        }
+    }
+
     tracing_subscriber::fmt::Subscriber::builder()
         .with_env_filter(env_filter)
         .with_writer(std::io::stderr)
