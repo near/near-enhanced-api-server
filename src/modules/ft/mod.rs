5bedf3958e1a4674340734577bcb6852d8d69ad1
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2022-12-15 13:19:00 +0400

    fix paths

--- src/modules/ft/mod.rs
+++ src/modules/ft/mod.rs
@@ -5,24 +5,16 @@ mod resources;
 mod schemas;
 
 pub(crate) fn register_services(app: &mut web::ServiceConfig) {
-    // I'll drop these comments, leaving it only because it's easier for the review
-    // app.service(
-    //     web::resource("/accounts/{account_id}/NEAR")
-    //         .route(web::get().to(resources::get_near_balance)),
-    // )
     app.service(
-        web::resource("/accounts/{account_id}/FT").route(web::get().to(resources::get_ft_balances)),
+        web::resource("/accounts/{account_id}/balances/FT")
+            .route(web::get().to(resources::get_ft_balances)),
     )
     .service(
-        web::resource("/accounts/{account_id}/FT/{contract_account_id}")
+        web::resource("/accounts/{account_id}/balances/FT/{contract_account_id}")
             .route(web::get().to(resources::get_ft_balance_by_contract)),
     )
-    // .service(
-    //     web::resource("/accounts/{account_id}/NEAR/history")
-    //         .route(web::get().to(resources::get_near_history)),
-    // )
     .service(
-        web::resource("/accounts/{account_id}/FT/{contract_account_id}/history")
+        web::resource("/accounts/{account_id}/balances/FT/{contract_account_id}/history")
             .route(web::get().to(resources::get_ft_history)),
     )
     .service(
--- src/modules/native/mod.rs
+++ src/modules/native/mod.rs
@@ -6,11 +6,11 @@ mod schemas;
 
 pub(crate) fn register_services(app: &mut web::ServiceConfig) {
     app.service(
-        web::resource("/accounts/{account_id}/NEAR")
+        web::resource("/accounts/{account_id}/balances/NEAR")
             .route(web::get().to(resources::get_near_balance)),
     )
     .service(
-        web::resource("/accounts/{account_id}/NEAR/history")
+        web::resource("/accounts/{account_id}/balances/NEAR/history")
             .route(web::get().to(resources::get_near_history)),
     );
 }
